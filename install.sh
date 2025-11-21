b09042652df450e0cfb046a0a6607a5557c84dfb
Jacob Ray Gilman <jacob@oscillas.com>
2025-11-21 10:23:10 -0600

    Create global gitignore and add it to the install.sh script

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ DOTFILES_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && p
 
 # Add the vimrc file
 [ ! -f "$HOME/.vimrc" ] && ln -s "$DOTFILES_LOCATION/.vimrc" "$HOME/.vimrc"
+
+# Set-up the global gitignore file
+[ ! -f "$HOME/.gitignore_global" ] && ln -s "$DOTFILES_LOCATION/.gitignore_global" "$HOME/.gitignore_global"
+git config --global core.excludesfile "$HOME/.gitignore_global"
