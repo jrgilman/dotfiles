97f60556733a5fa90c5a6dd870336ec980556f07
Jacob Ray Gilman <jacob@oscillas.com>
2025-11-21 10:22:46 -0600

    Check if symlink for vimrc already exists before recreating

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 
 DOTFILES_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-ln -s "$DOTFILES_LOCATION/.vimrc" "$HOME/.vimrc"
+# Add the vimrc file
+[ ! -f "$HOME/.vimrc" ] && ln -s "$DOTFILES_LOCATION/.vimrc" "$HOME/.vimrc"
